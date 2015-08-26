52767d0bc37974037f36c1c47bf671961f5c76df
Boyer <julien@meetserious.com>
2015-08-26 12:27:06 +0200

    ok

--- src/Tests/ProductTest.php
+++ src/Tests/ProductTest.php
@@ -19,6 +19,16 @@ class ProductTests extends \PHPUnit_Framework_TestCase{
         $product =null;
 
         try {
+            $stack = array();
+            $this->assertEquals(0, count($stack));
+
+            array_push($stack, 'foo');
+            $this->assertEquals('foo', $stack[count($stack)-1]);
+            $this->assertEquals(1, count($stack));
+
+            $this->assertEquals('foo', array_pop($stack));
+            $this->assertEquals(0, count($stack));
+            
 
             $product = new Product(2);
             $this->assertEquals(2, $product->getQuantity());
