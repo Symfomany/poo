7e0cb96666d82c57e8e3d3b0ce80c9cacc64a88b
Boyer <julien@meetserious.com>
2015-08-27 11:45:54 +0200

    add changes

--- src/App/Exceptions/StringException.php
+++ src/App/Exceptions/StringException.php
@@ -3,8 +3,6 @@
 namespace App\Exceptions;
 
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 /**
  * Class AvailableException.
@@ -14,10 +12,6 @@ class StringException extends \Exception
     public function __construct($message, $code = 0)
     {
         parent::__construct($message, $code);
-        // create a log channel
-        $log = new Logger('[String Exception]');
-        $log->pushHandler(new StreamHandler('logs/app.log', Logger::WARNING));
-        $log->addWarning("Un produit n'a pas une description correcte");
     }
 
     public function __toString()
